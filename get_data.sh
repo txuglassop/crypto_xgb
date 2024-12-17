46fbdcb49be796e573e90a8865dac37cd6ba97ae
txuglassop <t.xuglassop@gmail.com>
2024-12-17 18:56:31 +1100

    fixed bugs for data scraper and added more custom options

--- get_data.sh
+++ get_data.sh
@@ -1,30 +1,30 @@
 export STORE_DIRECTORY=./input
 
-if [ -z "$1" ]; then
-    # pass current wd as default directory
-    echo "Usage: $0 <ticker>"
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <ticker> <WINDOW> <YEAR>"
     exit 1
 fi
 
 TICKER="$1"
+WINDOW="$2"
+YEAR="$3"
 
-python3 scraper/download-kline.py -t spot -s ${TICKER} -i 1mo -y 2024
+python3 scraper/download-kline.py -t spot -s ${TICKER} -i ${WINDOW} -y ${YEAR}
 
 
 # unzip all files into /input
 echo "Unzipping files into $STORE_DIRECTORY..."
 
 find "$STORE_DIRECTORY/data" -type f -name "*.zip" | while read -r zip_file; do
-    echo "Unzipping: $zip_file"
     unzip -o "$zip_file" -d "$STORE_DIRECTORY"
 done
 
 # concatenate all csv files into one
-COMBINED_FILE="${STORE_DIRECTORY}/${TICKER}_final.csv"
+COMBINED_FILE="${STORE_DIRECTORY}/${TICKER}_${WINDOW}_${YEAR}_final.csv"
 
 > "$COMBINED_FILE"  # Create or truncate the combined file
 
-find "$STORE_DIRECTORY" -maxdepth 1 -type f -name "*${TICKER}*.csv" | sort | while read -r file; do
+find "$STORE_DIRECTORY" -maxdepth 1 -type f -name "*${TICKER}*.csv" ! -name "*final*.csv" | sort | while read -r file; do
     cat "$file" >> "$COMBINED_FILE"
 done
 
--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import sys
 
-def add_headers_kline(filename: str):
+def add_headers_kline(path: str):
     """
     adds the headers to each column in a kline dataset from binance, changing the 
-    original csv file
+    original csv file. also converts open_time from unix time to d/m/y
 
     params:
         str: filename - name of the csv file 
@@ -13,15 +13,19 @@ def add_headers_kline(filename: str):
         nothing 
     """
     col_names = [
-        'open_time', 'open', 'high', 'low', 'close', 'close_time',
+        'open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time',
         'base_asset_volume', 'no_trades', 'taker_buy_vol', 'taker_buy_base_asset_vol', 'ignore'
     ]
 
-    df = pd.read_csv(filename, header=None, names=col_names)
-    df = df.drop(columns=df.columns[-1])
+    df = pd.read_csv(path, header=None, names=col_names, index_col=False)
+    df['date'] = pd.to_datetime(df['open_time'], unit='ms').dt.strftime('%d/%m/%Y')
+    df = df.drop(columns=['ignore', 'open_time', 'close_time'])
 
-    df.to_csv(filename, index=False)
+    cols = ['date'] + [col for col in df.columns if col != 'date']
+    df = df[cols]
+
+    df.to_csv(path, index=False)
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
-    add_headers_kline(filename=filename)
+    path = sys.argv[1]
+    add_headers_kline(path=path)
