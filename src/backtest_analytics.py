cb7095b13807084584634b21f44b140390d7d1e5
txuglassop <t.xuglassop@gmail.com>
2025-01-18 10:00:21 +1100

    added position graph to plot_backtest and added get_trades, which will get entry/exit positions for all trades in a backtest

--- src/backtest_analytics.py
+++ src/backtest_analytics.py
@@ -15,7 +15,7 @@ def plot_backtest(backtest_results: pd.DataFrame, portion = [0, 100]):
         portion (list) - an array of len 2 that describes the portion of the 
                 backtest to be plotted
     """
-    if len(portion) != 2 or portion[1] <= portion[0]:
+    if len(portion) != 2 or portion[1] <= portion[0] or portion[0] < 0 or portion[1] > 100:
         raise ValueError('portion argument inappropriate.')
     
     start_index = int(portion[0] / 100 * backtest_results.shape[0])
@@ -25,7 +25,7 @@ def plot_backtest(backtest_results: pd.DataFrame, portion = [0, 100]):
     fig = make_subplots(rows=3, cols=1, shared_xaxes=True, 
                         vertical_spacing=0.1, 
                         row_heights=[0.4, 0.3, 0.3], 
-                        subplot_titles=("OHLC Chart of Test Data", "Portfolio Value", "Capital Over Time"))
+                        subplot_titles=("OHLC Chart of Test Data", "Portfolio Value", "Position"))
 
     # OHLC plot
     fig.add_trace(go.Ohlc(
@@ -47,13 +47,13 @@ def plot_backtest(backtest_results: pd.DataFrame, portion = [0, 100]):
         name='Current Portfolio Value'
     ), row=2, col=1)
 
-    # capital over time plot
+    # trades plot
     fig.add_trace(go.Scatter(
         x=df['timestamp'],
-        y=df['capital'],
+        y=df['trades'].cumsum(),
         mode='lines',
         line=dict(color='purple'),
-        name='Current Capital'
+        name='Trades'
     ), row=3, col=1)
 
     fig.update_layout(
@@ -64,8 +64,48 @@ def plot_backtest(backtest_results: pd.DataFrame, portion = [0, 100]):
 
     fig.update_yaxes(title_text="Price", row=1, col=1)
     fig.update_yaxes(title_text="Portfolio Value", row=2, col=1)
-    fig.update_yaxes(title_text="Capital", row=3, col=1)
+    fig.update_yaxes(title_text="Position", row=3, col=1)
     fig.update_xaxes(title_text="Time", row=3, col=1)
-
     
     fig.show()
+
+def trade_analyser(backtest_results: pd.DataFrame):
+    """
+    Given the backtest results (as returned from `run_backtest`), will
+    analyse all trades made throughout the test and return analytics
+    about them
+    """
+    col_names = ['trade_entry', 'trade_exit', 'entry_price', 'exit_price']
+    trades_df = pd.DataFrame(columns=col_names)
+
+    # keep track of which trades we have exited out of
+    exits = np.zeros(backtest_results.shape[0])
+    ongoing_trades = False
+
+    for idx1, row1 in backtest_results.iterrows():
+        # if there are ongoing trades, break 
+        if ongoing_trades:
+            break
+
+        # if there were no buys, continue
+        if row1['trades'] <= 0:
+            continue
+        
+        for _ in range(int(row1['trades'])):
+            # go through each of the trades made here and find next sell
+            for idx2, row2 in backtest_results[idx1:].iterrows():
+                if ongoing_trades:
+                    break
+                elif row2['trades'] < 0 and exits[idx2] != -row2['trades']:
+                    # we are exiting this trade - log it!
+                    exits[idx2] += 1
+                    trades_df.loc[len(trades_df)] = [
+                        row1['timestamp'], row2['timestamp'], row1['open'], row2['close']
+                    ]
+                    break
+                elif idx2 == len(exits):
+                    # we have reached the end of our test without exiting
+                    # all future buys are still ongoing
+                    ongoing_trades = True
+
+    return trades_df
