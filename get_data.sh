6ad960020aa17567627c2a363a175f74e0fbcd96
txuglassop <t.xuglassop@gmail.com>
2025-02-01 16:15:55 +1100

    fixed data scraper - it should now actually scrape data for all years, remove duplicate rows and order by timestamp if binance api does some funky stuff

--- get_data.sh
+++ get_data.sh
@@ -1,16 +1,20 @@
+#!/bin/bash
+
 export STORE_DIRECTORY=./input
 
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <ticker> <WINDOW> <YEAR>"
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 <ticker> <WINDOW> <START_YEAR> <END_YEAR>"
     exit 1
 fi
 
 TICKER="$1"
 WINDOW="$2"
-YEAR="$3"
+START_YEAR="$3"
+END_YEAR="$4"
 
-python3 scraper/download-kline.py -t spot -s ${TICKER} -i ${WINDOW} -y ${YEAR}
-
+for (( i=$START_YEAR; i<=$END_YEAR; i++ )); do
+    python3 scraper/download-kline.py -t spot -s ${TICKER} -i ${WINDOW} -y ${i}
+done
 
 # unzip all files into /input
 echo "Unzipping files into $STORE_DIRECTORY..."
@@ -20,7 +24,9 @@ find "$STORE_DIRECTORY/data" -type f -name "*.zip" | while read -r zip_file; do
 done
 
 # concatenate all csv files into one
-COMBINED_FILE="${STORE_DIRECTORY}/${TICKER}_${WINDOW}_${YEAR}_final.csv"
+# include '_final' in final file to signify this file should not be deleted or used
+# in truncation
+COMBINED_FILE="${STORE_DIRECTORY}/${TICKER}_${WINDOW}_${START_YEAR}_${END_YEAR}_final.csv"
 
 > "$COMBINED_FILE"  # Create or truncate the combined file
 
--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import numpy as np
 import sys
 
 def add_headers_kline(path: str):
     """
     adds the headers to each column in a kline dataset from binance, changing the 
-    original csv file. also converts open_time from unix time to d/m/y
+    original csv file. note `open_time` is renamed to `timestamp`
 
     params:
         str: path - path to the csv
@@ -18,15 +19,34 @@ def add_headers_kline(path: str):
     ]
 
     df = pd.read_csv(path, header=None, names=col_names, index_col=False)
-    #df['date'] = pd.to_datetime(df['open_time'], unit='ms').dt.strftime('%d/%m/%Y')
-    df['date'] = df['open_time']
+    df['timestamp'] = df['open_time']
     df = df.drop(columns=['ignore', 'open_time', 'close_time'])
 
-    cols = ['date'] + [col for col in df.columns if col != 'date']
+    cols = ['timestamp'] + [col for col in df.columns if col != 'timestamp']
     df = df[cols]
 
     df.to_csv(path, index=False)
 
+def sort_and_check_data(path: str):
+    """
+    ensures data is ordered in ascending order of the `timestamp` feature and removes any duplicates.
+    will then print all unique increments in `timestamp` as a way of confirming there are no missing
+    rows. any irregular / outstanding increments should be investigated
+
+    params:
+        path (str) - path to the csv
+    """
+    df = pd.read_csv(path)
+    df.drop_duplicates(inplace=True)
+    df.sort_values('timestamp', inplace=True)
+
+    incr = np.diff(df['timestamp'])
+    unique, counts = np.unique(incr, return_counts=True)
+
+    for u,c in zip(unique, counts):
+        print(f'Incr: {u}, Count: {c}')
+
 if __name__ == '__main__':
     path = sys.argv[1]
     add_headers_kline(path=path)
+    sort_and_check_data(path=path)
