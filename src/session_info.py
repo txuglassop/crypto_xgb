a940a64de39e07c5ac03f5f20bac747c7f3728e1
txuglassop <t.xuglassop@gmail.com>
2025-02-03 23:25:34 +1100

    added session info logger which will log key information and parameters of the sesssion in a specified directory

--- src/session_info.py
+++ src/session_info.py
@@ -1,4 +1,12 @@
-
+"""
+Use this class to keep track of session info and ultimately log backtest results
+with all key info
+"""
+import numpy as np
+from datetime import datetime
+from pathlib import Path
+from textwrap import fill
+import os
 
 class SessionInfo():
     def __init__(
@@ -26,3 +34,53 @@ class SessionInfo():
         self.small_down_margin = small_down_margin
         self.small_up_margin = small_up_margin
         self.big_up_margin = big_up_margin
+
+    def add_features(self, features: list):
+        self.features = features
+    
+    def add_strategy(self, strategy: str):
+        self.strategy = strategy
+
+    def add_backtest(self, backtest_results):
+        self.backtest_results = backtest_results
+
+    def _get_session_info_string(self):
+        features = fill(", ".join(self.features), width=50, subsequent_indent="")
+        string = f"""
+================== Session Info ==================
+
+Data:                           {self.data}
+Num Classes:                    {self.num_classes}
+Lag Factor:                     {self.lag_factor}
+Test Size:                      {self.test_size}
+
+Up Margin:                      {self.up_margin}
+Down Margin:                    {self.down_margin}
+
+
+-------------------- Features --------------------
+
+{features}
+
+"""
+        return string
+
+    def log_session(self, path_to_output: str):
+        """
+        logs all session information, predictors, and back test results in a directory
+        within the specified path
+
+        params:
+            `path_to_output` (str) - a path to where the session is to be logged
+        """
+        # make new directory
+        os.makedirs(path_to_output, exist_ok=True)
+
+        time = datetime.now().strftime('%H:%M:%S')
+        filename = self.strategy + time + '.txt'
+        results_path = os.path.join(path_to_output, filename)
+
+        with open(results_path, 'w') as results:
+            results.write(self._get_session_info_string())
+
+
