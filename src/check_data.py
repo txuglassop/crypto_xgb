46dca80d4e96b9def01e94b4c33318ce0fceb768
txuglassop <t.xuglassop@gmail.com>
2025-02-15 14:07:34 +1100

    data check now includes time since last entry

--- src/check_data.py
+++ src/check_data.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 import sys
+import time
 
 def check_data(path: str):
     """
     runs some checks on the data given by `path`. this includes
     checking if the `timestamp` feature is ordered, the increments
     within the `timestamp` feature and their frequency, any duplicate
-    rows
+    rows, time since last update
 
     params:
         path (str): path to the file (data)
@@ -25,6 +26,17 @@ def check_data(path: str):
     else:
         print('Data is NOT sorted - please check timestamp feature')
 
+    last_entry_point = df['timestamp'].iloc[-1]
+    time_since = time.time()*1000 - last_entry_point
+    seconds = time_since // 1000
+    minutes = seconds // 60
+    hours = minutes // 60
+    seconds %= 60
+    minutes %= 60
+
+    print('Time since last entry:')
+    print(f'{hours} hours, {minutes} minutes, {seconds} seconds\n')
+
     duplicates = df.duplicated()
     num_dup = np.sum(duplicates)
     print(f'There are {num_dup} duplicate rows\n')
@@ -34,6 +46,7 @@ def check_data(path: str):
 
     for u,c in zip(unique, counts):
         print(f'Incr: {u}, Count: {c}')
+
 
 if __name__ == '__main__':
     path = sys.argv[1]
